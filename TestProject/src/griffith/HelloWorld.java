6ff8bc842ca60c4c146739ef9bfe20e50dbc7ea6
Samuel Simeon <simeonlekan0@gmail.com>
2024-02-27 00:09:04 +0100

    second commit

--- TestProject/src/griffith/HelloWorld.java
+++ TestProject/src/griffith/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World!" to the console.
         System.out.println("Hello, World!");
+        System.out.println("how are you doing!");
+        
     }
 }
