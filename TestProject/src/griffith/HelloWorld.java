5c3e034342f662bfe13180a5527651de437a83c5
Samuel Simeon <simeonlekan0@gmail.com>
2024-02-26 23:52:19 +0100

    initial commit

---
+++ TestProject/src/griffith/HelloWorld.java
@@ -0,0 +1,8 @@
+package griffith;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
